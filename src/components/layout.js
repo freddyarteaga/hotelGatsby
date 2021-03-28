06606a49f9c92562b98d33697d42d4e79700abc6
Freddy Arteaga <freddy.artea@gmail.com>
2021-03-28 17:51:45 -0500

    add: nav styled

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,19 +18,19 @@ const Layout = props => {
                         line-height: 10.5;
                     }
                     h1, h2, h3 {
-                        margin: 0
-                        line-height: 1.5
+                        margin: 0;
+                        line-height: 1.5;
                     }
                     h1, h2{
                         font-family: 'Roboto', serif;
                     }
                     h3 {
-                        font-family: 'PT Sans', sans-serif
+                        font-family: 'PT Sans', sans-serif;
                     }
                     ul {
-                        list-style: none
-                        margin: 0
-                        padding: 0
+                        list-style: none;
+                        margin: 0;
+                        padding: 0;
                     }
                 }
             `}
